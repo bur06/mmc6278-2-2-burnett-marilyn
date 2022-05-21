3a16fab19535523f567087f8104dae6352039d7d
Marilyn Burnett <burnett.marilyn@ufl.edu>
2022-05-21 15:19:02 -0500

    Greet String to Number

--- greet.js
+++ greet.js
@@ -8,24 +8,15 @@ function greeting() {
         prompt("Hi!" + inputAge);
     }
 
-    var ageInt = parseInt(inputAge);
     if(!inputAge) return; //cancels without input
-    var yrBorn = Math.floor(2022 - ageInt);
-    alert("You were born in " + yrBorn + "!");
+    var ageNum = parseInt(inputAge);
+    console.log(typeof ageNum);
+//    var today = new Date();
+  //  var nowYr = (today.getFullYear());
+
+    //var yrBorn = nowYr - ageInt;
+    //console.log(yrBorn);
+    //alert("You were born in " + yrBorn + "!");
 }
 
-greeting()
-
-
-function rollDice() {
-    var inputString = prompt("How many sides does the dice have?");
-    var inputNum = parseInt(inputString);
-    if(!inputNum) return;
-    var rollNum = Math.floor(Math.random() * inputNum) + 1;
-    alert("Your roll is " + rollNum);
-    var rollAgain = confirm("Would like to roll again?");
-
-    if (rollAgain) {
-        return rollDice();
-    }
-}
+greeting();
