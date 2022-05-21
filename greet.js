1422c5d7b17adbbd4aaf56272f64213f1835eb0f
Marilyn Burnett <burnett.marilyn@ufl.edu>
2022-05-21 12:47:07 -0500

    Greet Prompt

---
+++ greet.js
@@ -0,0 +1,8 @@
+var userName = prompt("Hello! What is your name?");
+var userAge = ("What is your age?");
+
+if (userName) {
+    prompt("Hi, " + userName + "! " + userAge);
+} else {
+    prompt("Hi!" + userAge);
+}
