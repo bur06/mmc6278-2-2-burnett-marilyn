22cec56255c277d33b40edcfeabd40078004a3b2
Marilyn Burnett <burnett.marilyn@ufl.edu>
2022-05-22 16:53:16 -0500

    Index js parseInt

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 function greet() {
     var userName = prompt("What is your name?");
     alert("Hello, " + userName + "!");
+    var userAge = prompt("How old are you?");
+    alert(parseInt(userAge));
 }
